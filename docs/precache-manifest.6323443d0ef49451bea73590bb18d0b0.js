565ecd9d945a0556e2ab6ea1a7716d73a2229007
Alex <alexcantongarcia97@gmail.com>
2020-10-09 19:42:05 +0200

    Build rename docs

---
+++ docs/precache-manifest.6323443d0ef49451bea73590bb18d0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960da39cca6a49a271afc0cace9fbcd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b89da61c8562b9719a3e",
+    "url": "/static/css/main.fbe91274.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89da61c8562b9719a3e",
+    "url": "/static/js/main.5856733d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
